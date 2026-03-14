3a97316e42ab5f56b5af0cf74dfee7db72279533
LankaHarsha <lharsha4@gmail.com>
2026-03-14 23:44:07 +0530

    changed code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   console.log(`Worker ID ${workerId} is handling this request!`);
 
   // Send to browser
-  res.send(`Hello World! This is V2.`);
+  res.send(`Hello World! This is V3.`);
 });
 
 const PORT = process.env.PORT || 3000;
